97b3d29a99275bacde753f9e2e344cdc034ce305
Daniel Knaack <daniel.knaack@protonmail.com>
2025-01-06 11:47:12 +0100

    impl: Inline the headers

--- main.cpp
+++ main.cpp
@@ -6,14 +6,298 @@
 
 #include <iostream>
 
+// Matrix is column-major
 template<typename T>
-struct matrix;
+struct matrix {
+	T *at;
+	size_t num_rows;
+	size_t num_cols;
+
+	T &operator()(int row, int col)
+	{
+		assert(col < num_cols && row < num_rows);
+		return at[row * num_cols + col];
+	}
+};
+
+struct ratio {
+	int num, denom;
+
+	ratio(int _num = 0, int _denom = 1);
+};
+
+static int gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+static int xgcd(int a, int b, int &x, int &y)
+{
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+
+    int x1, y1;
+    int gcd = xgcd(b, a % b, x1, y1);
+
+    x = y1, y = x1 - (a / b) * y1;
+    return gcd;
+}
+
+ratio::ratio(int _num, int _denom)
+{
+	num = _num;
+	denom = _denom;
+
+	if (denom < 0) {
+		num = -num;
+		denom = -denom;
+	}
+
+	int g = gcd(abs(num), denom);
+	num /= g;
+	denom /= g;
+}
+
+static ratio operator-(ratio r)
+{
+	return ratio{-r.num, r.denom};
+}
+
+static ratio operator+(ratio lhs, ratio rhs)
+{
+	int num = lhs.num * rhs.denom + rhs.num * lhs.denom;
+	int denom = lhs.denom * rhs.denom;
+	return ratio{num, denom};
+}
+
+static ratio operator-(ratio lhs, ratio rhs)
+{
+	int num = lhs.num * rhs.denom - rhs.num * lhs.denom;
+	int denom = lhs.denom * rhs.denom;
+	return ratio{num, denom};
+}
+
+static ratio operator*(ratio lhs, ratio rhs)
+{
+	int num = lhs.num * rhs.num;
+	int denom = lhs.denom * rhs.denom;
+	return ratio{num, denom};
+}
+
+static ratio operator/(ratio lhs, ratio rhs)
+{
+	int num = lhs.num * rhs.denom;
+	int denom = lhs.denom * rhs.num;
+	return ratio{num, denom};
+}
+
+static bool operator==(ratio lhs, ratio rhs)
+{
+	bool result = (lhs.num == rhs.num && lhs.denom == rhs.denom);
+	return result;
+}
+
+static bool operator!=(ratio lhs, ratio rhs)
+{
+	return !(lhs == rhs);
+}
+
+static bool operator<(ratio lhs, ratio rhs)
+{
+	return (lhs.num * rhs.denom < rhs.num * lhs.denom);
+}
+
+static bool operator>(ratio lhs, ratio rhs)
+{
+	return (lhs.num * rhs.denom > rhs.num * lhs.denom);
+}
+
+static bool operator<=(ratio lhs, ratio rhs)
+{
+	return (lhs.num * rhs.denom <= rhs.num * lhs.denom);
+}
+
+static bool operator>=(ratio lhs, ratio rhs)
+{
+	return (lhs.num * rhs.denom >= rhs.num * lhs.denom);
+}
+
+static ratio &operator+=(ratio &lhs, ratio rhs)
+{
+	return lhs = lhs + rhs;
+}
+
+static ratio &operator-=(ratio &lhs, ratio rhs)
+{
+	return lhs = lhs - rhs;
+}
+
+static ratio &operator*=(ratio &lhs, ratio rhs)
+{
+	return lhs = lhs * rhs;
+}
+
+static ratio &operator/=(ratio &lhs, ratio rhs)
+{
+	return lhs = lhs / rhs;
+}
+
+static ratio frac(ratio r)
+{
+	int num = r.num % r.denom;
+	int denom = r.denom;
+	if (num < 0) {
+		num += r.denom;
+	}
+
+	return ratio{num, denom};
+}
+
+static int floor(ratio r)
+{
+	int num = abs(r.num) / r.denom; // division always rounds to zero
+	if (r.num < 0) {
+		num = -num;
+	}
+
+	return num;
+}
+
+static int round(ratio r)
+{
+	int result = floor(r);
+	if (2 * frac(r) > r.denom) {
+		result += 1;
+	}
+
+	return result;
+}
+
+static ratio abs(ratio r)
+{
+	return r > 0 ? r : -r;
+}
 
 template<typename T>
-static void print_linear_system(matrix<T> A, matrix<T> b);
+static matrix<T> zeros(size_t num_rows, size_t num_cols)
+{
+	matrix<T> result;
+	result.at = new T[num_rows * num_cols]{};
+	result.num_rows = num_rows;
+	result.num_cols = num_cols;
+	return result;
+}
 
-#include "ratio.h"
-#include "matrix.h"
+template<typename T>
+static matrix<T> random_matrix(size_t num_rows, size_t num_cols)
+{
+	matrix<T> result;
+	result.at = new T[num_rows * num_cols];
+	result.num_rows = num_rows;
+	result.num_cols = num_cols;
+
+	for (size_t i = 0; i < num_rows * num_cols; i++) {
+		result.at[i] = rand() % 100 + 1;
+	}
+
+	return result;
+}
+
+template<typename T>
+static matrix<T> read_matrix(FILE *f, size_t num_rows, size_t num_cols)
+{
+	matrix result = zeros<T>(num_rows, num_cols);
+
+    for (size_t i = 0; i < num_rows; i++) {
+        for (size_t j = 0; j < num_cols; j++) {
+			int num;
+			fscanf(f, "%d", &num);
+			result(i, j) = num;
+        }
+    }
+
+	return result;
+}
+
+template<typename T>
+static void swap_rows(matrix<T> m, size_t i, size_t j)
+{
+	for (size_t k = 0; k < m.num_cols; k++) {
+		T tmp = m(i, k);
+		m(i, k) = m(j, k);
+		m(j, k) = tmp;
+	}
+}
+
+template<typename T>
+static matrix<T> clone(matrix<T> m)
+{
+	matrix<T> result;
+	result.at = new T[m.num_cols * m.num_rows]();
+	result.num_cols = m.num_cols;
+	result.num_rows = m.num_rows;
+	memcpy(result.at, m.at, m.num_cols * m.num_rows * sizeof(T));
+	return result;
+}
+
+template<typename T>
+static void clear(matrix<T> m)
+{
+	for (int i = 0; i < m.num_cols * m.num_rows; i++) {
+		m.at[i] = 0;
+	}
+}
+
+template<typename T>
+static void solve(matrix<T> A, matrix<T> x, matrix<T> b)
+{
+	// Matrix must be square matrix.
+	assert(A.num_cols == A.num_rows && A.num_rows == b.num_rows);
+    int n = A.num_cols;
+
+	A = clone(A);
+	b = clone(b);
+
+    // Forward elimination
+    for (int i = 0; i < n; ++i) {
+        // Find the maximum element in the column i for partial pivoting
+        int max_row = i;
+        for (int k = i + 1; k < n; k++) {
+            if (abs(A(k, i)) > abs(A(max_row, i))) {
+				max_row = k;
+            }
+        }
+
+        // Swap the maximum row with the current row
+        if (max_row != i) {
+            swap_rows(A, i, max_row);
+            swap_rows(b, i, max_row);
+        }
+
+        // Make the diagonal element 1 and reduce below rows
+        for (int j = i + 1; j < n; ++j) {
+            T factor = A(j, i) / A(i, i);
+            for (int k = i; k < n; ++k) {
+                A(j, k) -= factor * A(i, k);
+            }
+
+            b(j, 0) -= factor * b(i, 0);
+        }
+    }
+
+    // Back substitution
+    for (int i = n - 1; i >= 0; --i) {
+        x(i, 0) = b(i, 0);
+
+        for (int j = i + 1; j < n; ++j) {
+            x(i, 0) -= A(i, j) * x(j, 0);
+        }
+
+        x(i, 0) /= A(i, i);
+    }
+}
 
 static float frac(float f)
 {
